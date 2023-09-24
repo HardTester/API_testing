1a8a7aabe2676f3dc4ba041ff26284b95e352796
var.aleks <var.aleks@engee.cloud>
2023-09-24 15:18:36 +0500

    рефактор - logs improved

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,23 @@ import logging
 from dotenv import load_dotenv
 
 from utilities.files_utils import get_project_path
+from utilities.logger_utils import logger
 
 
 def pytest_configure(config):
     # загружаем переменные-параметры из файла /.env
     load_dotenv(dotenv_path=get_project_path() + '/.env')
 
-    # устанавливаем формат логов
-    params = {
-        "level": logging.INFO,
-        "format": "%(lineno)d: %(asctime)s %(message)s",
-        "filename": f"{get_project_path()}/logs/info.log", "filemode": "w"
-    }
-    logging.basicConfig(**params)
+    # задаем паарметры логгера
+    file_handler = logging.FileHandler(f"{get_project_path()}/logs/info.log", "w")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter("%(lineno)d: %(asctime)s %(message)s"))
+
+    # создаем кастомный логгер
+    custom_logger = logging.getLogger("custom_loger")
+    custom_logger.setLevel(logging.INFO)
+    custom_logger.addHandler(file_handler)
+
+
+def pytest_runtest_setup(item):
+    logger.info(f"{item.name}:")
--- utilities/logger_utils.py
+++ utilities/logger_utils.py
@@ -1,3 +1,3 @@
 import logging
 
-logger = logging.getLogger()
+logger = logging.getLogger("custom_loger")
