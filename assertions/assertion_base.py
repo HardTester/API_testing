479bb686af9b8994728c18905b3aaa7026b1a3d3
var.aleks <var.aleks@engee.cloud>
2023-09-24 19:40:21 +0500

    рефактор - add assert schema

--- assertions/assertion_base.py
+++ assertions/assertion_base.py
@@ -124,7 +124,12 @@ def assert_schema(response, model: Type[BaseModel]):
     :param model: модель, по которой будет проверяться схема json
     :raises ValidationError: если тело ответа не соответствует схеме
     """
-    model.model_validate(response.json(), strict=True)
+    body = response.json()
+    if isinstance(body, list):
+        for item in body:
+            model.model_validate(item, strict=True)
+    else:
+        model.model_validate(body, strict=True)
 
 
 def assert_left_in_right_json(response, exp_json, actual_json):
--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,13 +1,13 @@
 from http import HTTPStatus
 
 import pytest
+from assertions.objects_assertion import should_be_posted_success, should_be_updated_success, should_be_deleted_success, \
+    should_be_valid_objects_response
 
 from api.api_client import ApiClient
 from api.objects_api import get_objects, get_object, post_object, put_object, delete_object
 from assertions.assertion_base import assert_status_code, assert_response_body_fields, assert_bad_request, \
     assert_not_found, assert_empty_list, assert_schema, assert_not_exist
-from assertions.objects_assertion import should_be_posted_success, should_be_updated_success, should_be_deleted_success, \
-    should_be_valid_objects_response
 from models.object_models import ObjectOutSchema, ObjectCreateOutSchema, CustomObjCreateOutSchema, \
     ObjectUpdateOutSchema, CustomObjUpdateOutSchema
 from utilities.files_utils import read_json_test_data, read_json_common_request_data
@@ -45,6 +45,7 @@ class TestObjects:
 
         # убеждаемся, что в ответ пришли именно те объекты, id которых мы запросили
         assert_status_code(response, HTTPStatus.OK)
+        assert_schema(response, ObjectOutSchema)
         should_be_valid_objects_response(request, response, param)
 
     def test_get_objects_not_exist_id(self, client):
